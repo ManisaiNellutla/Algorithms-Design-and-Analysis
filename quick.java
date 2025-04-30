f18cc9719fab410ca82401aa122a75f8934b9eab
Nellutla Manisai <manisainellutla1432@gmail.com>
2025-04-30 18:03:53 +0530

    added Q

---
+++ quick.java
@@ -0,0 +1,52 @@
+
+
+public class quick{
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, low, j); // Place pivot in correct position
+        return j;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Main method to test the quicksort
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 2, 5, 7, 9, 1, 3};
+        quickSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+   }
+
+
+
+
+
+}
